4740b8c04b4eed310ab3711fe235d949ce7a9f93
kosouhov <vladimir@kosouhov.com>
2018-09-21 01:46:14 +0300

    add canvas and images

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,16 @@
 import '../styles/index.scss';
+
+let CANVAS = document.getElementById('canvas'),
+	CONTEXT = getContext('2d'),
+	bird = new Image(),
+	floor = new Image(),
+	pipeTop = new Image(),
+	pipeBottom = new Image(),
+	background = new Image();
+
+bird.src = '../images/bird-center.png';
+floor.src = '../images/floor.png';
+pipeTop.src = '../images/pipe-top.png';
+pipeBottom.src = '../images/pipe-bottom.png';
+background.src = '../images/background.png';
+
